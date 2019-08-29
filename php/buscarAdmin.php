fa3d5388cc3569f71d18f66fc033bd5e21e3c2e9
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-08-29 17:46:57 -0500

    Update buscarAdmin.php
    agregarle la conexion.php

--- php/buscarAdmin.php
+++ php/buscarAdmin.php
@@ -1,9 +1,5 @@
 <?PHP
-$hostname_localhost="localhost";
-$database_localhost="readandwatch";
-$username_localhost="root";
-$password_localhost="";
-$conexion = new mysqli($hostname_localhost, $username_localhost, $password_localhost, $database_localhost); 
+include 'conexion.php';
 $json=array();
  
 /*Comprobar si existe el usuario o no*/
