fdcdf324d39a584015338921b6c10b29f6cb04a9
skillerhero <skillerhero@users.noreply.github.com>
2019-09-02 22:56:08 -0500

    Update cargarVidDoc.php

--- php/cargarVidDoc.php
+++ php/cargarVidDoc.php
@@ -1,15 +1,17 @@
 <?PHP
 include 'conexion.php';
 $json=array();
-
-/*Cargar los comentarios de video*/
+            $consulta="SELECT * from viddoc WHERE idTema= ".$idTema." and tipo='".$tipo."'";
+            echo $consulta;
+/*Cargar los comentarios de video
         if(isset($_GET["idTema"]) && isset($_GET["tipo"])){
             $idTema=$_GET["idTema"];
             $tipo=$_GET["tipo"];
             $consulta="SELECT * from viddoc WHERE idTema= ".$idTema." and tipo='".$tipo."'";
+            echo $consulta;
             $resultado=mysqli_query($conexion, $consulta);
             //$rowCount=mysqli_num_rows($resultado);
-            echo $consulta;
+            
             while($r=mysqli_fetch_array($resultado)){
                 //$status=array("existente"=>1,"cantidad"=>$rowCount,);
                 $json['usuario'][] = $r; 
@@ -23,4 +25,5 @@ $json=array();
             mysqli_close($conexion);
             echo json_encode($json);
         }
+        */
 ?>
