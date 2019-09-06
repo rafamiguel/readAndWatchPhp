b5dc60175e27a3ad502fa511377160f1f0826888
skillerhero <skillerhero@users.noreply.github.com>
2019-09-06 01:10:47 -0500

    Update listaTemas.php

--- php/listaTemas.php
+++ php/listaTemas.php
@@ -1,13 +1,3 @@
 <?PHP
-include 'conexion.php';
-$json=array();
-/*Cargar la lista de temas*/
-if($_GET("materia")){
-	echo $_GET("materia");
-
-    }
-
-    mysqli_close($conexion);
-    echo json_encode($json);
-}
+echo "Hola";
 ?>
