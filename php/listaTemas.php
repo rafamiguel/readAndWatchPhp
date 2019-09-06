f1c9b1111e576804962a649617bf0139e0d03bc6
skillerhero <skillerhero@users.noreply.github.com>
2019-09-06 01:22:08 -0500

    Update listaTemas.php

--- php/listaTemas.php
+++ php/listaTemas.php
@@ -1,5 +1,18 @@
 <?PHP
 include 'conexion.php';
+$json=array();
 
+echo "Hola";
+/*Cargar la lista de temas*/
+if(isset($_GET("materia"))){
+    $consulta="select tema.nombre from tema inner join materia on tema.idMatera=materia.idMateria where materia.nombre=";
+    $resultado=mysqli_query($conexion, $consulta);
+    while($r=mysqli_fetch_array($resultado)){
+        $json['usuario'][] = $r; 
+    }
+
+    
+    echo json_encode($json);
+}
 	mysqli_close($conexion);
 ?>
