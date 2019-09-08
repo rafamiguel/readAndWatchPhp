4a2db898dcff0dc15f9dbef61759e6575932eb3c
skillerhero <skillerhero@users.noreply.github.com>
2019-09-08 15:25:59 -0500

    Update cargarVidDoc.php

--- php/cargarVidDoc.php
+++ php/cargarVidDoc.php
@@ -6,7 +6,7 @@ $json=array();
         if(isset($_GET["idTema"]) && isset($_GET["tipo"])){
             $idTema=$_GET["idTema"];
             $tipo=$_GET["tipo"];
-            $consulta="select *from viddoc WHERE idTema= ".$idTema." and tipo='".$tipo."'";
+            $consulta="select *from viddoc WHERE idSubtema= ".$idTema." and tipo='".$tipo."'";
             $resultado=mysqli_query($conexion, $consulta);
             //$rowCount=mysqli_num_rows($resultado);
             
