37116747a19abf28023036a2451557cb517452fb
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-09-22 19:58:39 -0500

    Create insertarPregunta.php

---
+++ php/insertarPregunta.php
@@ -0,0 +1,19 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+/*Se insertará en la bd las preguntas que se realizan*/
+if(isset($_GET["idPregunta"]) && isset($_GET["titulo"]) && isset($_GET["descripcion"])&& isset($_GET["idReportesPreg"]) && isset($_GET["idUsuario"])){
+
+	$idPregunta=$_GET["idPregunta"];
+    $titulo=$_GET["titulo"];
+    $descripcion=$_GET["descripcion"];
+    $idReportesPreg=$_GET["idReportesPreg"];
+    $idUsuario=$_GET["idUsuario"];
+
+    $sentencia="insert into pregunta 
+    (idPregunta,titulo,descripcion,idReportesPreg,idUsuario) values({$idPregunta},'{$titulo}', '{$descripcion}',{$idReportesPreg},{$idUsuario}) ";
+    echo $sentencia;
+    $resultado=mysqli_query($conexion, $sentencia);
+	}
+	mysqli_close($conexion);
+?>
