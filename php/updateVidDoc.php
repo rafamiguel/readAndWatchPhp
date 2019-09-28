eae3b7ff66d846f7c6152e206505aea98b423173
skillerhero <skillerhero@users.noreply.github.com>
2019-09-28 00:36:50 -0500

    Create updateVidDoc.php

---
+++ php/updateVidDoc.php
@@ -0,0 +1,25 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+;
+/*Cargar los comentarios de video*/
+if(isset($_GET["idTema"]) && isset($_GET["tipo"]) && isset($_GET["descripcion"])&& isset($_GET["ruta"]) && isset($_GET["fechaSubida"]) && isset($_GET["idUsuario"])){
+    $idTema=$_GET["idTema"];
+    $tipo=$_GET["tipo"];
+    $descripcion=$_GET["descripcion"];
+    $ruta=$_GET["ruta"];
+    $fechaSubida=$_GET["fechaSubida"];
+    $idUsuario=$_GET["idUsuario"];
+    if(tipo=='v'){
+    $sentencia="update viddoc where idVidDoc={$idVidDoc} set
+    tipo='{$tipo}',descripcion='{$descripcion}',rutaImagen='@drawable/miniatura',ruta='{$ruta}',fechaSubida='{$fechaSubida}',visitas=0,idUsuario={$idUsuario},idSubtema={$idTema},idReporte=1";
+    }else{
+    $sentencia="update viddoc where idVidDoc={$idVidDoc} set
+    tipo='{$tipo}',descripcion='{$descripcion}',rutaImagen='@drawable/miniatura',ruta='{$ruta}',fechaSubida='{$fechaSubida}',visitas=0,idUsuario={$idUsuario},idSubtema={$idTema},idReporte=1";
+    }
+    echo $sentencia;
+    $resultado=mysqli_query($conexion, $sentencia);
+    //echo json_encode($json);
+}
+mysqli_close($conexion);
+?>
