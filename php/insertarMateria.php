49f08270db784b84200aa5c60b527a754966aedd
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-09-29 02:44:58 -0500

    Update insertarMateria.php

--- php/insertarMateria.php
+++ php/insertarMateria.php
@@ -6,10 +6,22 @@ $json=array();
     if(isset($_GET["nombre"]) && isset($_GET["foto"])){
         $nombre=$_GET["nombre"];
         $rutaImagen=$_GET["rutaImagen"];
-        $consulta="INSERT INTO materia(nombre,rutaImagen,votos,idUsuario) values('{$nombre}','{$foto}',0, 1);"  
-       echo $consulta;
-       $resultado=mysqli_query($conexion, $consulta);
+        $consulta="INSERT INTO materia(nombre,rutaImagen,votos,idUsuario) values(".$nombre.","$.$rutaImagen.",0, 1)";   
+         
+       if($resultado=mysqli_query($conexion, $consulta)){
+            $json['usuario'][] =array("exito"=>1,);
+        }else{
+          $resulta['usuario']=array("exito"=>-1,);
+          $json['usuario'][]=$resulta;
+        }
+
+        mysqli_close($conexion);
+        echo json_encode($json);
     }
-    mysqli_close($conexion);
+    else{
+      $resulta["exito"]=0;
+      $json['usuario'][]=$resulta;
+      echo json_encode($json);
+    }
     
 ?>
