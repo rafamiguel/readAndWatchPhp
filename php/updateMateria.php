fcea523ce290c80b223fa1747e857ac3fb931b77
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-09-29 18:04:07 -0500

    Update updateMateria.php

--- php/updateMateria.php
+++ php/updateMateria.php
@@ -1,7 +1,7 @@
 <?PHP
 include 'conexion.php';
 $json=array();
-;
+
 /*Cargar los comentarios de video*/
 if(isset($_GET["idMateria"]) && isset($_GET["nombre"]) && isset($_GET["rutaImagen"])){
     $idMateria=$_GET["idMateria"];
@@ -12,9 +12,13 @@ if(isset($_GET["idMateria"]) && isset($_GET["nombre"]) && isset($_GET["rutaImage
     $sentencia="update materia set
     nombre='{$nombre}',rutaImagen='{$rutaImagen}' where idMateria = {$idMateria}";
      //update materia set nombre = "Psico", rutaImagen = "ruta" where idMateria = 9;
+    if(mysqli_query($conexion, $sentencia)){
+    $json['usuario'][]=array("success" => 1,);
+    else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;}
     }
     //echo $sentencia;
-    $resultado=mysqli_query($conexion, $sentencia);
-    //echo json_encode($json);
+    echo json_encode($json);
 mysqli_close($conexion);
 ?>
