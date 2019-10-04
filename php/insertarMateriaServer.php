61fa5171f4a856816de542960b25055170738fa8
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-03 20:33:01 -0500

    Create insertarMateriaServer.php

---
+++ php/insertarMateriaServer.php
@@ -0,0 +1,32 @@
+<?PHP
+include 'conexion.php';
+
+
+/*Comprobar si se ha hecho un comentario antes*/
+   
+        $nombre=$_POST["nombre"];
+        $rutaImagen=$_POST["rutaImagen"];
+        $path = "imagenes/$nombre.jpg";
+        $url= "https://files.000webhost.com/$path"
+
+        file_put_contents($path, base64_decode($rutaImagen));
+        $bytesArchivo=file_get_contents($path);
+        //$consulta="INSERT INTO materia(nombre,rutaImagen,votos,idUsuario) values('".$nombre."','".$rutaImagen."',0, 1)";   
+
+       /*if($resultado=mysqli_query($conexion, $consulta)){
+            $json['usuario'][] =array("exito"=>1,);
+        }else{
+          $resulta['usuario']=array("exito"=>-1,);
+          $json['usuario'][]=$resulta;
+        } */
+
+       // mysqli_close($conexion);
+        //echo json_encode($json);
+  
+   /* else{
+      $resulta["exito"]=0;
+      $json['usuario'][]=$resulta;
+      echo json_encode($json);
+    }
+    */
+?>
