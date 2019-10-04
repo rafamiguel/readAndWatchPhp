696acf687f1b41c7208814afe58b81af3c8f2a7f
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-03 22:29:16 -0500

    Update insertarMateriaServer.php

--- php/insertarMateriaServer.php
+++ php/insertarMateriaServer.php
@@ -2,33 +2,15 @@
 include 'conexion.php';
 
 
-/*Comprobar si se ha hecho un comentario antes*/
    
         $nombre=$_POST["nombre"];
         $rutaImagen=$_POST["rutaImagen"];
         $path = "imagen/$nombre.jpg";
-        $url= "https://readandwatch.000webhostapp.com/$path"
+        $url= "https://readandwatch.000webhostapp.com/$path";
 
         file_put_contents($path, base64_decode($rutaImagen));
         $bytesArchivo=file_get_contents($path);
 
         echo "registra";
-        //$consulta="INSERT INTO materia(nombre,rutaImagen,votos,idUsuario) values('".$nombre."','".$rutaImagen."',0, 1)";   
-
-       /*if($resultado=mysqli_query($conexion, $consulta)){
-            $json['usuario'][] =array("exito"=>1,);
-        }else{
-          $resulta['usuario']=array("exito"=>-1,);
-          $json['usuario'][]=$resulta;
-        } */
-
-       // mysqli_close($conexion);
-        //echo json_encode($json);
-  
-   /* else{
-      $resulta["exito"]=0;
-      $json['usuario'][]=$resulta;
-      echo json_encode($json);
-    }
-    */
+       
 ?>
