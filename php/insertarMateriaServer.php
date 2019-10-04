e0b816191ba550bbbc78e604076486d84ca46bf4
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-03 22:37:39 -0500

    Update insertarMateriaServer.php

--- php/insertarMateriaServer.php
+++ php/insertarMateriaServer.php
@@ -6,7 +6,7 @@ include 'conexion.php';
         $nombre=$_POST["nombre"];
         $rutaImagen=$_POST["rutaImagen"];
         $path = "imagen/$nombre.png";
-        $url= "https://readandwatch.000webhostapp.com/$path";
+        $url= "https://readandwatch.000webhostapp.com/?dir=./$path";
 
         file_put_contents($path, base64_decode($rutaImagen));
         $bytesArchivo=file_get_contents($path);
