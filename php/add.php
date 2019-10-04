0ed64fed5a277577a9b5ccf16817239f3487350f
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-04 08:29:07 -0500

    prueba de subir imagen con php

---
+++ php/add.php
@@ -0,0 +1,19 @@
+ <?php
+
+   $imagepic = $_FILES["logo"]["name"];
+   echo $imagepic;
+   $tempimgloc = $_FILES["logo"]["tmp_name"];
+   echo $tempimgloc;
+   $errorimg = $_FILES["logo"]["error"];
+   echo $errorimg;
+
+   if($errorimg > 0)
+   {  
+      echo "<strong> <font size='18'>There was a problem uploading your Logo. Please try again!</font></strong>";
+  echo "<BR>";
+   }
+   else 
+   {
+      move_uploaded_file($tempimgloc, "https://readandwatch.000webhostapp.com/imagen/".$imagepic);
+   }
+?>
