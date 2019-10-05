e63b0b9b958f78b50b16f14c3023ae680861ee6c
skillerhero <skillerhero@users.noreply.github.com>
2019-10-05 14:41:57 -0500

    Update add.php

--- php/add.php
+++ php/add.php
@@ -1,11 +1,11 @@
 <?php
 $target_dir = "https://readandwatch.000webhostapp.com/?dir=imagen/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-}
+
+$file = basename($_FILES["fileToUpload"]["name"];
+$dest = fopen("ftp://readandwatch:Vergademono1@000webhost.com/" . $file, "wb");
+$src = file_get_contents($file);
+fwrite($dest, $src, strlen($src));
+fclose($dest); 
+
 ?>
