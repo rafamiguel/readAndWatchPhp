3aa9c7f6c6d60047d5699d101b3304085ebd1005
skillerhero <skillerhero@users.noreply.github.com>
2019-10-05 15:46:04 -0500

    Update add.php

--- php/add.php
+++ php/add.php
@@ -1,24 +1,16 @@
 <?php
-$ftp_server="files.000webhost.com";
-$ftp_usuario="readandwatch";
-$ftp_pass="Vergademono1";
-$con_id=ftp_connect($ftp_server);
-$lr=ftp_login($con_id, $ftp_usuario, $ftp_pass);
-if ((!$con_id) || (!$lr) ) {
-    echo 'No se pudo conectar.';
-    exit;
-} else {
-    echo 'Conectado correctamente.';
-    $temp=explode(".", $_FILES['archivo']['name']);
-    $source_file=$_FILES['archivo']['tmp_name'];
-    $destino="archivos";
-    $nombre=$_FILES["archivo"]['name'];
-    //ftp_pass($con_id, true);
-    $subio=ftp_put($con_id, $destino.'/'.$nombre, $source_file, FTP_BINARY);
-    if ($subio) {
-        echo "Se subió el archivo correctamente."
-    } else {
-        echo "Ocurrió un error.";
-    }
-}
+// connect and login data
+$web = 'files.000webhost.com';
+$user = 'readandwatch';
+$pass = 'Vergademono1';
+// file location
+$server_file = '/public_html/imagen/'.$_FILES["archivo"]['name'];
+$local_file = $_FILES['archivo']['tmp_name'];
+//connect
+$conn_id = ftp_connect($web);
+$login_result = ftp_login($conn_id,$user,$pass);
+//uploading
+if (ftp_put($conn_id, $server_file, $local_file, FTP_BINARY))
+ {echo "Success";} 
+else {echo "Failed";}
 ?>
