93adee2e907804469e28a950ce32063c2a1667dc
skillerhero <skillerhero@users.noreply.github.com>
2019-10-05 14:46:16 -0500

    Update add.php

--- php/add.php
+++ php/add.php
@@ -3,7 +3,7 @@ $target_dir = "https://readandwatch.000webhostapp.com/?dir=imagen/";
 $uploadOk = 1;
 
 $file = basename($_FILES["fileToUpload"]["name"];
-$dest = fopen("ftp://readandwatch:Vergademono1@000webhost.com/" . $file, "wb");
+$dest = fopen("ftp://readandwatch:Vergademono1@files.000webhost.com/" . $file, "wb");
 $src = file_get_contents($file);
 fwrite($dest, $src, strlen($src));
 fclose($dest); 
