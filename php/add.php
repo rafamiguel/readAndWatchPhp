f3dd37cc0feed1763641d187bf289d7e20408be6
skillerhero <skillerhero@users.noreply.github.com>
2019-10-05 15:27:11 -0500

    Update add.php

--- php/add.php
+++ php/add.php
@@ -1,12 +1,3 @@
 <?php
-	$ftp_server="files.000webhost.com";
-	$ftp_usuario="readandwatch";
-	$ftp_password="Vergademono1";
-	$conexion=ftp_connect("files.000webhost.com") or die ("No se pudo conectar");
-	$resultado=ftp_login($conexion, $ftp_usuario, $ftp_password);
-	if(!$resultado || !$conexion){
-		echo 'No se pudo conectar';
-	}else{
-		echo 'Conectado correctamente';
-	}
+	echo "Hola";
 ?>	
