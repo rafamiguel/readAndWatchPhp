812bedf9bfaf0177163e814aab53d617ff4f8d54
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-11 00:52:54 -0500

    Update buscarEstudianteEliminar.php

--- php/buscarEstudianteEliminar.php
+++ php/buscarEstudianteEliminar.php
@@ -9,28 +9,18 @@ $json=array();
 
             $consulta="SELECT nombre, apellidos, rutaFoto from usuario WHERE nombre = {$nombre} and apellidos = {$apellidos}";  
             $resultado=mysqli_query($conexion, $consulta);
-            
+
             if($registro=mysqli_fetch_array($resultado)){
                 $json['usuario'][] =$registro; 
             }else{
-              $resulta["correo"]='';
-              $resulta["contrasena"]='';
               $resulta["nombre"]='';
-              $resulta["apellidos"]='';
-              $resulta["telefono"]='';
-              $resulta["descripcion"]='';
+              
    
               $json['usuario'][]=$resulta;
             }
 
             mysqli_close($conexion);
             echo json_encode($json);
-        }
-        else{
-          $resulta["success"]=0;
-          $resulta["message"]='WS no retorna';
-          $json['usuario'][]=$resulta;
-          echo json_encode($json);
         }
 /*Comprobar si existe el usuario o no*/
  
