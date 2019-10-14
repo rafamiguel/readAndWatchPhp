bedb118525be7a9aa6f65dfe11e55b732e2e6afe
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-14 08:55:05 -0500

    Update deleteTema.php

--- php/deleteTema.php
+++ php/deleteTema.php
@@ -4,7 +4,7 @@ $json=array();
 
 /*Comprobar si se ha hecho un comentario antes*/
     if(isset($_GET["idTema"])){
-        $idSubtema=$_GET['idTema'];
+        $idTema=$_GET['idTema'];
         $consulta="DELETE FROM tema where idTema={$idTema}";  
         if($resultado=mysqli_query($conexion, $consulta)){
             $json['usuario'][] =array("exito"=>1,);
