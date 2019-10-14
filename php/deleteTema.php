7607604ada7525deb7efe1965d2664bb19350683
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-14 08:24:40 -0500

    Create deleteTema.php

---
+++ php/deleteTema.php
@@ -0,0 +1,24 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+
+/*Comprobar si se ha hecho un comentario antes*/
+    if(isset($_GET["idTema"])){
+        $idSubtema=$_GET['idTema'];
+        $consulta="DELETE FROM tema where idTema={$idTema}";  
+        if($resultado=mysqli_query($conexion, $consulta)){
+            $json['usuario'][] =array("exito"=>1,);
+        }else{
+          $resulta['usuario']=array("exito"=>-1,);
+          $json['usuario'][]=$resulta;
+        }
+
+        mysqli_close($conexion);
+        echo json_encode($json);
+    }
+    else{
+      $resulta["exito"]=0;
+      $json['usuario'][]=$resulta;
+      echo json_encode($json);
+    }
+?>
