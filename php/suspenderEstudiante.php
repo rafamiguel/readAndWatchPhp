ebce4e8972c7a70b50a7d4379e36ac18c3844d3c
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-13 19:17:18 -0500

    Update suspenderEstudiante.php

--- php/suspenderEstudiante.php
+++ php/suspenderEstudiante.php
@@ -4,7 +4,7 @@ $json=array();
  
 
 /*UPDATE usuario set estado='N' where nombre ='Ariana' and apellidos = 'Espinoza' and tipo='E';*/
-        if( isset($_GET["nombre"]) && isset($_GET["apellidos"])){
+        if(isset($_GET["nombre"]) && isset($_GET["apellidos"])){
             $nombre=$_GET["nombre"];
             $apellidos=$_GET["apellidos"];
 
@@ -12,7 +12,7 @@ $json=array();
             if(mysqli_query($conexion, $sentencia)){
                 $json['usuario'][]=array("success" => 1,
                 "contrasena" => $contrasena, "nombre" => $nombre, "apellidos" => $apellidos,
-                "telefono" => $telefono, "descripcion" => $descripcion,);
+                "telefono" => $telefono, "descripcion" => $descripcion, $tipo => "tipo");
                 echo json_encode($json);
             }else{
               $resulta["success"]=0;
