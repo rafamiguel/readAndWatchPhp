51e5ee4147b32ff500d2d6f39cfe7da644c8461b
skillerhero <skillerhero@users.noreply.github.com>
2019-10-14 20:30:39 -0500

    Update reportarVidDoc.php

--- php/reportarVidDoc.php
+++ php/reportarVidDoc.php
@@ -11,28 +11,7 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["tipo"]) && isset($_GET["idUsuario"])
     $result = $conexion->query($consulta);
     $row = $result->fetch_row();
     echo $row[0];
-    if($row[0]==0)
-        $reportes=mysqli_result(mysqli_query("SELECT count(*) from personaReportaVidDoc where idVidDoc={$idVidDoc}"), 0);
-        $reportes++;
-        $sentencia="insert into reportesviddoc(reportes,tipo,idVidDoc) values({$reportes},'{$tipo}',{$idVidDoc})";
-        echo $sentencia;
-        $resultado=mysqli_query($conexion, $sentencia);
-        $sentencia="insert into personaReportaVidDoc(idVidDoc,idUsuario) values({$idVidDoc},{$idUsuario})";
-        echo $sentencia;
-        $resultado=mysqli_query($conexion, $sentencia);
-        $resulta["repetido"]=0;
-        $json['usuario'][]=$resulta;
-        $resulta["success"]=1;
-        $json['usuario'][]+=$resulta;
-        echo json_encode($json);
-    }else{
-        $resulta["repetido"]=1;
-        $json['usuario'][]=$resulta;
-        $resulta["success"]=1;
-        $json['usuario'][]+=$resulta;
-        echo json_encode($json);
-
-    }
+   
 
 }else{
         $resulta["repetido"]=0;
