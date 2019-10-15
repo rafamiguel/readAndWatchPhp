e48188e273b88cf329cf5dbd931e499ac8cd86e7
skillerhero <skillerhero@users.noreply.github.com>
2019-10-14 20:52:59 -0500

    Update reportarVidDoc.php

--- php/reportarVidDoc.php
+++ php/reportarVidDoc.php
@@ -6,10 +6,33 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["tipo"]) && isset($_GET["idUsuario"])
     $idVidDoc=$_GET["idVidDoc"];
     $tipo=$_GET["tipo"];
     $idUsuario=$_GET["idUsuario"];
-    $consulta="SELECT count(*) from personaReportaVidDoc where idUsuario={$idUsuario}";
+    $consulta="SELECT * from personaReportaVidDoc where idUsuario={$idUsuario}";
     echo $consulta;
     echo "<br>".mysqli_num_rows($conexion->query($consulta));
-   
+    if(mysqli_num_rows($conexion->query($consulta))==0)
+        $consulta="SELECT *from personaReportaVidDoc where idVidDoc={$idVidDoc}";
+        echo $consulta;
+        $reportes=mysqli_num_rows($conexion->query($consulta))
+        $reportes++;
+        $sentencia="insert into reportesviddoc(reportes,tipo,idVidDoc) values({$reportes},'{$tipo}',{$idVidDoc})";
+        echo $sentencia;
+        $resultado=mysqli_query($conexion, $sentencia);
+        $sentencia="insert into personaReportaVidDoc(idVidDoc,idUsuario) values({$idVidDoc},{$idUsuario})";
+        echo $sentencia;
+        $resultado=mysqli_query($conexion, $sentencia);
+        $resulta["repetido"]=0;
+        $json['usuario'][]=$resulta;
+        $resulta["success"]=1;
+        $json['usuario'][]+=$resulta;
+        echo json_encode($json);
+    }else{
+        $resulta["repetido"]=1;
+        $json['usuario'][]=$resulta;
+        $resulta["success"]=1;
+        $json['usuario'][]+=$resulta;
+        echo json_encode($json);
+
+    }
 
 }else{
         $resulta["repetido"]=0;
