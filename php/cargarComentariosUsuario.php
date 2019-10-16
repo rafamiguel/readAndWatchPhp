d26eb4e1ac0291172b0845112f4e39672c1e8ba7
skillerhero <skillerhero@users.noreply.github.com>
2019-10-16 00:12:13 -0500

    update php buscar comentarios user

--- php/cargarComentariosUsuario.php
+++ php/cargarComentariosUsuario.php
@@ -5,18 +5,19 @@ if(isset($_GET["idUsuario"])){
 	$idUsuario=$_GET["idUsuario"];
 	$tipo=$_GET["tipo"];
 	$consulta="select idComentario from comentario WHERE idUsuario={$idUsuario}";
+ 	echo $consulta;
 	$resultado=mysqli_query($conexion, $consulta);
-
+	echo "<br>".$resultado;
   	while($r=mysqli_fetch_array($resultado)){
         $json['usuario'][] = $r; 
   	}
 
 	$consulta="select idVidDoc from viddoc WHERE idUsuario={$idUsuario} and idPregunta IS NOT NULL";
-	echo $consulta;
+	echo "<br".$consulta."<br>";
 	$resultado=mysqli_query($conexion, $consulta);
-
+	echo "<br>".$resultado;
   	while($r=mysqli_fetch_array($resultado)){
-        $json['usuario'][] += $r; 
+        $json['usuario'][] = $r; 
   	}
 
     mysqli_close($conexion);
