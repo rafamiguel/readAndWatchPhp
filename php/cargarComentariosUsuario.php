2698f02510d25a5327e98f0489245233e7c7d27d
skillerhero <skillerhero@users.noreply.github.com>
2019-10-15 23:52:00 -0500

    Update cargarComentariosUsuario.php

--- php/cargarComentariosUsuario.php
+++ php/cargarComentariosUsuario.php
@@ -7,14 +7,19 @@ if(isset($_GET["idUsuario"])){
 	$consulta="select idComentario from comentario WHERE idUsuario={$idUsuario}";
 	$resultado=mysqli_query($conexion, $consulta);
 
-	  while($r=mysqli_fetch_array($resultado)){
-                
-                $json['usuario'][] = $r; 
-                
-            }
+  	while($r=mysqli_fetch_array($resultado)){
+        $json['usuario'][] = $r; 
+  	}
 
-            mysqli_close($conexion);
-            echo json_encode($json);
+	$consulta="select idVidDoc from viddoc WHERE idUsuario={$idUsuario} and idPregunta IS NOT NULL";
+	$resultado=mysqli_query($conexion, $consulta);
+	
+  	while($r=mysqli_fetch_array($resultado)){
+        $json['usuario'][] += $r; 
+  	}
+
+    mysqli_close($conexion);
+    echo json_encode($json);
 }
 
 ?>
