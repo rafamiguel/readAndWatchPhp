a9a13bc4a257a04c17d1002a965965c98cbce218
skillerhero <skillerhero@users.noreply.github.com>
2019-10-17 20:45:13 -0500

    Update reportarVidDoc.php

--- php/reportarVidDoc.php
+++ php/reportarVidDoc.php
@@ -16,12 +16,10 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["tipo"]) && isset($_GET["idUsuario"])
         $resultado=mysqli_query($conexion, $sentencia);
         $sentencia="insert into personaReportaVidDoc(idVidDoc,idUsuario) values({$idVidDoc},{$idUsuario})";
         $resultado=mysqli_query($conexion, $sentencia);
-        echo "exito";
         $json['usuario'][]=array("repetido" => 0,);
         $json['usuario'][]=array("success" => 1,);
         echo json_encode($json);
     }else{
-        echo "repetido";
         $json['usuario'][]=array("repetido" => 1,);
         $json['usuario'][]=array("success" => 1,);
         echo json_encode($json);
@@ -29,7 +27,6 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["tipo"]) && isset($_GET["idUsuario"])
     }
 
 }else{
-    echo "error";
     $json['usuario'][]=array("repetido" => 0,);
     $json['usuario'][]=array("success" => 0,);
     echo json_encode($json);
