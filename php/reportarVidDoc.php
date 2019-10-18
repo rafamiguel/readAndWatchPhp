2ce98eac78ae8bf8b1806e41aaa53379d74328ce
skillerhero <skillerhero@users.noreply.github.com>
2019-10-17 20:48:16 -0500

    Update reportarVidDoc.php

--- php/reportarVidDoc.php
+++ php/reportarVidDoc.php
@@ -16,19 +16,19 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["tipo"]) && isset($_GET["idUsuario"])
         $resultado=mysqli_query($conexion, $sentencia);
         $sentencia="insert into personaReportaVidDoc(idVidDoc,idUsuario) values({$idVidDoc},{$idUsuario})";
         $resultado=mysqli_query($conexion, $sentencia);
-        $json['usuario'][]=array("repetido" => 0,);
-        $json['usuario'][]=array("success" => 1,);
+        $json['usuario'][]=array("repetido" => FALSE,);
+        $json['usuario'][]=array("success" => TRUE,);
         echo json_encode($json);
     }else{
-        $json['usuario'][]=array("repetido" => 1,);
-        $json['usuario'][]=array("success" => 1,);
+        $json['usuario'][]=array("repetido" => TRUE,);
+        $json['usuario'][]=array("success" => TRUE,);
         echo json_encode($json);
 
     }
 
 }else{
-    $json['usuario'][]=array("repetido" => 0,);
-    $json['usuario'][]=array("success" => 0,);
+    $json['usuario'][]=array("repetido" => FALSE,);
+    $json['usuario'][]=array("success" => FALSE,);
     echo json_encode($json);
 }
 mysqli_close($conexion);
