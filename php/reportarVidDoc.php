fe3c4fda9f917f0d585eb3e9c31ad20b305f648a
skillerhero <skillerhero@users.noreply.github.com>
2019-10-17 23:53:48 -0500

    Update reportarVidDoc.php

--- php/reportarVidDoc.php
+++ php/reportarVidDoc.php
@@ -10,25 +10,26 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["tipo"]) && isset($_GET["idUsuario"])
     and idVidDoc={$idVidDoc}";
     $repetido= mysqli_num_rows($conexion->query($consulta));
     if($repetido==0){
+        $reportes=0;
         $consulta="SELECT reportes from reportesviddoc where idVidDoc={$idVidDoc} and tipo='{$tipo}'";
         if($resultado=mysqli_query($conexion, $sentencia)){
             while($row = mysqli_fetch_array($resultado)){
                     $reportes = $row['reportes'];
                     $reportes++;
-                }
-            if($reportes==1){
-                $sentencia="insert into reportesviddoc(reportes,tipo,idVidDoc) values({$reportes},'{$tipo}',{$idVidDoc})";
-                $resultado=mysqli_query($conexion, $sentencia);
-            }else{
-                $sentencia="update reportesviddoc  set reportes={$reportes} where idVidDoc={$idVidDoc}";
-                $resultado=mysqli_query($conexion, $sentencia);
             }
-            $sentencia="insert into personaReportaVidDoc(idVidDoc,idUsuario) values({$idVidDoc},{$idUsuario})";
-            $resultado=mysqli_query($conexion, $sentencia);
-            $json['usuario'][]=array("repetido" => FALSE,);
-            $json['usuario'][]=array("success" => TRUE,);
-            echo json_encode($json);
         }
+        if($reportes==0){
+            $sentencia="insert into reportesviddoc(reportes,tipo,idVidDoc) values({$reportes},'{$tipo}',{$idVidDoc})";
+            $resultado=mysqli_query($conexion, $sentencia);
+        }else{
+            $sentencia="update reportesviddoc  set reportes={$reportes} where idVidDoc={$idVidDoc}";
+            $resultado=mysqli_query($conexion, $sentencia);
+        }
+        $sentencia="insert into personaReportaVidDoc(idVidDoc,idUsuario) values({$idVidDoc},{$idUsuario})";
+        $resultado=mysqli_query($conexion, $sentencia);
+        $json['usuario'][]=array("repetido" => FALSE,);
+        $json['usuario'][]=array("success" => TRUE,);
+        echo json_encode($json);
     }else{
         $json['usuario'][]=array("repetido" => TRUE,);
         $json['usuario'][]=array("success" => TRUE,);
