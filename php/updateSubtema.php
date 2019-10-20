6dcb464e20ee5ffda11ddbb8a005e92fe119fbbb
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-20 18:04:34 -0500

    Create updateSubtema.php

---
+++ php/updateSubtema.php
@@ -0,0 +1,23 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+
+/*Cargar los comentarios de video*/
+if(isset($_GET["idSubtema"]) && isset($_GET["nombre"])){
+    $idMateria=$_GET["idSubtema"];
+    $nombre=$_GET["nombre"]; 
+   
+    $sentencia="update subtema set
+    nombre='{$nombre}' where idSubtema = {$idSubtema}";
+     //update materia set nombre = "Psico", rutaImagen = "ruta" where idMateria = 9;
+    if(mysqli_query($conexion, $sentencia)){
+    $json['usuario'][]=array("success" => 1,);
+
+} else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;}
+    }
+    //echo $sentencia;
+    echo json_encode($json);
+mysqli_close($conexion);
+?>
