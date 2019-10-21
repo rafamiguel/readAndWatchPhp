9e956848794e5960d8dd75821fd811ae22486bc9
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-20 19:52:18 -0500

    Create insertarSubtema.php

---
+++ php/insertarSubtema.php
@@ -0,0 +1,32 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+ 
+/*Comprobar si existe el usuario o no*/
+        if(isset($_GET["idSubtema"]) && isset($_GET["idUsuario"]) && isset($_GET["nombre"])){
+            $idSubtema=$_GET["idSubtema"];
+             $idUsuario=$_GET["idUsuario"];
+              $nombre=$_GET["nombre"];
+            $consulta="insert into subtema (nombre,votos,idTema,idUsuario) values ('".$nombre."',0, ".$idTema.",".$idUsuario.")";
+            $resultado=mysqli_query($conexion, $consulta);
+            if($registro=mysqli_fetch_array($resultado)){
+                $json['usuario'][] =$registro; 
+            }else{
+            
+             $resulta["idTema"]='';
+   
+              $json['usuario'][]=$resulta;
+            }
+
+            mysqli_close($conexion);
+            echo json_encode($json);
+        }
+        else{
+          $resulta["success"]=0;
+          $resulta["message"]='WS no retorna';
+          $json['usuario'][]=$resulta;
+          echo json_encode($json);
+        }
+/*Comprobar si existe el usuario o no*/
+ 
+?>
