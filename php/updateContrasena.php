69fa32c0e65414da36d200916e20b2f54dfb77bd
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-21 09:27:09 -0500

    Create updateContrasena.php

---
+++ php/updateContrasena.php
@@ -0,0 +1,23 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+
+/*Cargar los comentarios de video*/
+if(isset($_GET["nueva"]) && isset($_GET["idUsuario"])){
+    $contrasena=$_GET["nueva"];
+    $idUsuario=$_GET["idUsuario"];
+   
+    $sentencia="update usuario set
+     contrasena ='{$contrasena}' where idUsuario = {$idUsuario}";
+     //update materia set nombre = "Psico", rutaImagen = "ruta" where idMateria = 9;
+    if(mysqli_query($conexion, $sentencia)){
+    $json['usuario'][]=array("success" => 1,);
+
+} else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;}
+    }
+    //echo $sentencia;
+    echo json_encode($json);
+mysqli_close($conexion);
+?>
