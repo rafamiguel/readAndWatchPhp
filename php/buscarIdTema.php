7a7e7fd8434dce4b801644cd977f630b0bf5eb1c
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-20 19:03:23 -0500

    Create buscarIdTema.php

---
+++ php/buscarIdTema.php
@@ -0,0 +1,30 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+ 
+/*Comprobar si existe el usuario o no*/
+        if(isset($_GET["idSubtema"])){
+            $idSubtema=$_GET["idSubtema"];
+            $consulta="SELECT idTema from subtema WHERE idSubtema = {$idSubtema}";  
+            $resultado=mysqli_query($conexion, $consulta);
+            if($registro=mysqli_fetch_array($resultado)){
+                $json['usuario'][] =$registro; 
+            }else{
+            
+             $resulta["idTema"]='';
+   
+              $json['usuario'][]=$resulta;
+            }
+
+            mysqli_close($conexion);
+            echo json_encode($json);
+        }
+        else{
+          $resulta["success"]=0;
+          $resulta["message"]='WS no retorna';
+          $json['usuario'][]=$resulta;
+          echo json_encode($json);
+        }
+/*Comprobar si existe el usuario o no*/
+ 
+?>
