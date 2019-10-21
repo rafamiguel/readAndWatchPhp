956cc1d96e567fbd7eef81c62d5106f765b49e89
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-21 09:44:46 -0500

    Update updateContrasena.php

--- php/updateContrasena.php
+++ php/updateContrasena.php
@@ -12,16 +12,16 @@ if(isset($_GET["contrasena"]) && isset($_GET["idUsuario"])){
      //update materia set nombre = "Psico", rutaImagen = "ruta" where idMateria = 9;
     if(mysqli_query($conexion, $sentencia)){
     $json['usuario'][]=array("success" => 1,);
-    echo $sentencia;
+  
 
 } else{
           $resulta["success"]=0;
           $json['usuario'][]=$resulta;
-          echo "error";
+          
         }
     }
     else{
-      echo "error2";
+      
     }
     //echo $sentencia;
     echo json_encode($json);
