30edc51552c15413cec6934cdec9b4e0084a0d6b
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-20 20:19:13 -0500

    Create updateTema.php

---
+++ php/updateTema.php
@@ -0,0 +1,23 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+
+/*Cargar los comentarios de video*/
+if(isset($_GET["idTema"]) && isset($_GET["nombre"])){
+    $idTema=$_GET["idTema"];
+    $nombre=$_GET["nombre"];
+   
+    $sentencia="update tema set
+    nombre='{$nombre}' idTema = {$idTema}";
+     
+    if(mysqli_query($conexion, $sentencia)){
+    $json['usuario'][]=array("success" => 1,);
+
+} else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;}
+    }
+    //echo $sentencia;
+    echo json_encode($json);
+mysqli_close($conexion);
+?>
