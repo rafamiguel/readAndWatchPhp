abf65b984156c1a006f720731c4f4c48444f38b3
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-24 18:56:22 -0500

    Create updateContrasenaCorreo.php

---
+++ php/updateContrasenaCorreo.php
@@ -0,0 +1,28 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+
+/*Cargar los comentarios de video*/
+if(isset($_GET["correo"]) && isset($_GET["contrasena"])){
+    $contrasena=$_GET["contrasena"];
+    $correo=$_GET["correo"];
+   
+    $sentencia="update usuario set
+     contrasena ='{$contrasena}' where correo = '{$correo}'";
+     //update materia set nombre = "Psico", rutaImagen = "ruta" where idMateria = 9;
+    if(mysqli_query($conexion, $sentencia)){
+    $json['usuario'][]=array("success" => 1,);
+ 
+} else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;
+          
+        }
+    }
+    else{
+      
+    }
+    //echo $sentencia;
+    echo json_encode($json);
+mysqli_close($conexion);
+?>
