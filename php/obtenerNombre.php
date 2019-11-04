71fd65a22f9a08769b1554ee4e2474e7ec936a41
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-03 18:46:47 -0600

    Create obtenerNombre.php

---
+++ php/obtenerNombre.php
@@ -0,0 +1,28 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+ 
+/*Comprobar si existe el usuario o no*/
+        if(isset($_GET["idUsuario"])){
+            $idUsuario=$_GET["idUsuario"];
+            $consulta="SELECT nombre from usuario WHERE idUsuario = {$idUsuario} and tipo='E'";  
+            $resultado=mysqli_query($conexion, $consulta);
+            if($registro=mysqli_fetch_array($resultado)){
+                $json['usuario'][] =$registro; 
+            }else{
+              
+              $json['usuario'][]=$resulta;
+            }
+
+            mysqli_close($conexion);
+            echo json_encode($json);
+        }
+        else{
+          $resulta["success"]=0;
+          $resulta["message"]='WS no retorna';
+          $json['usuario'][]=$resulta;
+          echo json_encode($json);
+        }
+/*Comprobar si existe el usuario o no*/
+ 
+?>
