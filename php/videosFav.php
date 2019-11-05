03ec28c8109dd103830ad32a72397eaf047e1bc6
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-05 01:17:57 -0600

    Update videosFav.php

--- php/videosFav.php
+++ php/videosFav.php
@@ -4,7 +4,7 @@ include 'conexion.php';
 $json=array();
 if(isset($_GET["idUsuario"])){
 	$idUsuario=$_GET["idUsuario"];
-	$consulta="SELECT viddoc.idUsuario, viddoc.descripcion, viddoc.rutaImagen, favorito.idVidDoc FROM viddoc INNER JOIN favorito ON favorito.idVidDoc = viddoc.idVidDoc WHERE favorito.idUsuario = {$idUsuario} and viddoc.tipo='v'";
+	$consulta="SELECT viddoc.idUsuario, viddoc.descripcion, viddoc.rutaImagen, viddoc.ruta, favorito.idVidDoc FROM viddoc INNER JOIN favorito ON favorito.idVidDoc = viddoc.idVidDoc WHERE favorito.idUsuario = {$idUsuario} and viddoc.tipo='v'";
 	$resultado=mysqli_query($conexion, $consulta);
 
 	  while($r=mysqli_fetch_array($resultado)){
