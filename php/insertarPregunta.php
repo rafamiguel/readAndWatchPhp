3d717f7cca56060b2a000f5019ee6ad8a20cc68c
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-07 01:00:04 -0600

    Update insertarPregunta.php

--- php/insertarPregunta.php
+++ php/insertarPregunta.php
@@ -2,16 +2,17 @@
 include 'conexion.php';
 $json=array();
 /*Se insertará en la bd las preguntas que se realizan*/
-if(isset($_GET["idPregunta"]) && isset($_GET["titulo"]) && isset($_GET["descripcion"]) && isset($_GET["idUsuario"])){
+if(isset($_GET["idPregunta"]) && isset($_GET["titulo"]) && isset($_GET["descripcion"])  && isset($_GET["eliminado"]) && isset($_GET["idUsuario"])){
 
 	$idPregunta=$_GET["idPregunta"];
     $titulo=$_GET["titulo"];
     $descripcion=$_GET["descripcion"];
     $idReportesPreg=$_GET["idReportesPreg"];
     $idUsuario=$_GET["idUsuario"];
+    $eliminado=$_GET["eliminado"];
 
     $sentencia="insert into pregunta 
-    (idPregunta,titulo,descripcion,idUsuario) values({$idPregunta},'{$titulo}', '{$descripcion}',{$idUsuario}) ";
+    (idPregunta,titulo,descripcion,eliminado,idUsuario) values({$idPregunta},'{$titulo}', '{$descripcion}','{$eliminado}',{$idUsuario}) ";
     echo $sentencia;
     $resultado=mysqli_query($conexion, $sentencia);
 	}
