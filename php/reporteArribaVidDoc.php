da54d3ea80bd629318c26b4d29b34c76a204d085
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-10 17:40:03 -0600

    Update reporteArribaVidDoc.php

--- php/reporteArribaVidDoc.php
+++ php/reporteArribaVidDoc.php
@@ -5,7 +5,8 @@ $json=array();
     if(isset($_GET["idVidDoc"])){
 
     $idVidDoc=$_GET["idVidDoc"];
-    $sentencia="UPDATE viddoc as a INNER JOIN reportesviddoc as b on a.{$idVidDoc} = b.{$idVidDoc} set a.eliminado = 'S' where b.reportes >= 3 and b.tipo = 'Contenido sexual u obseno' ";
+    echo $idVidDoc;
+    $sentencia="UPDATE viddoc as a INNER JOIN reportesviddoc as b on a.{.$idVidDoc.} = b.{$idVidDoc} set a.eliminado = 'S' where b.reportes >= 3 and b.tipo = 'Contenido sexual u obseno' ";
      echo $sentencia;
     if(mysqli_query($conexion, $sentencia)){
     $json['usuario'][]=array("success" => 1,);
