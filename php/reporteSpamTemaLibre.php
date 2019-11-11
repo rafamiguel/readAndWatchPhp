f954adb1347b6e4c7939b1a8d175000a79d5aabd
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-10 20:58:28 -0600

    Create reporteSpamTemaLibre.php

---
+++ php/reporteSpamTemaLibre.php
@@ -0,0 +1,22 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+
+    if(isset($_GET["idPregunta"])){
+
+    $idPregunta=$_GET["idPregunta"];
+    
+    $sentencia="UPDATE pregunta as a INNER JOIN reportespreg as b on a.idVidDoc = b.idVidDoc set a.eliminado = 'S' where b.reportes >= 5 and b.tipo = 'Es spam' ";
+    // echo $sentencia;
+    if(mysqli_query($conexion, $sentencia)){
+    $json['usuario'][]=array("success" => 1,);
+
+} else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;}
+    
+    //echo $sentencia;
+    echo json_encode($json);
+}
+mysqli_close($conexion);
+?>
