db7d9c6a8af08080c3bc097272b83415041687d5
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-10 22:07:09 -0600

    Create eliminarPerfilReporte.php

---
+++ php/eliminarPerfilReporte.php
@@ -0,0 +1,22 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+
+    if(isset($_GET["idPerfil"])){
+
+    $idPerfil=$_GET["idPerfil"];
+    
+    $sentencia="delete usuario, reportesperfil from usuario INNER JOIN reportesperfil on usuario.idPerfil = reportesperfil.idPerfil where reportesperfil.reportes >= 3 and reportesperfil.tipo = 'Contenido sexual u obseno' ";
+    //echo $sentencia;
+    if(mysqli_query($conexion, $sentencia)){
+    $json['usuario'][]=array("success" => 1,);
+
+} else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;}
+    
+    //echo $sentencia;
+    echo json_encode($json);
+}
+mysqli_close($conexion);
+?>
