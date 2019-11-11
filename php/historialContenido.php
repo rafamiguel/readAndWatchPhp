c53122e20c717dee34c42aa7447a16d9e03d8efa
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-11 01:35:17 -0600

    Update historialContenido.php

--- php/historialContenido.php
+++ php/historialContenido.php
@@ -6,7 +6,9 @@ $json=array();
         if(isset($_GET["idUsuario"])){
             $idUsuario=$_GET["idUsuario"];
 
-            $consulta="SELECT viddoc.ruta, viddoc.rutaImagen, reportesviddoc.tipo, reportesviddoc.idCastigo from viddoc INNER JOIN reportesviddoc on viddoc.idVidDoc = reportesviddoc.idVidDoc where viddoc.eliminado='S' and viddoc.idUsuario = {$idUsuario}";  
+/*SELECT viddoc.ruta, viddoc.rutaImagen, reportesviddoc.tipo, castigo.castigo, reportesviddoc.idCastigo from viddoc INNER JOIN reportesviddoc on viddoc.idVidDoc = reportesviddoc.idVidDoc INNER JOIN castigo on reportesviddoc.idCastigo = castigo.idCastigo where viddoc.eliminado='S' and viddoc.idUsuario =32 */
+
+            $consulta="SELECT viddoc.ruta, viddoc.rutaImagen, reportesviddoc.tipo, castigo.castigo, reportesviddoc.idCastigo from viddoc INNER JOIN reportesviddoc on viddoc.idVidDoc = reportesviddoc.idVidDoc INNER JOIN castigo on reportesviddoc.idCastigo= castigo.idCastigo where viddoc.eliminado='S' and viddoc.idUsuario = {$idUsuario}";  
             $resultado=mysqli_query($conexion, $consulta);
             while($registro=mysqli_fetch_array($resultado)){
                 $json['usuario'][] =$registro; 
