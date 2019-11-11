dc58017339fe3757072cfb7381a5428e27df826f
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-10 19:08:58 -0600

    gg

--- php/reporteArribaVidDoc.php
+++ php/reporteArribaVidDoc.php
@@ -8,10 +8,12 @@ $json=array();
     
     $sentencia="UPDATE viddoc as a INNER JOIN reportesviddoc as b on a.idVidDoc = b.idVidDoc set a.eliminado = 'S' where b.reportes >= 3 and b.tipo = 'Contenido sexual u obseno' ";
     // echo $sentencia;
-    while(mysqli_query($conexion, $sentencia)){
+    if(mysqli_query($conexion, $sentencia)){
     $json['usuario'][]=array("success" => 1,);
 
-}
+} else{
+          $resulta["success"]=0;
+          $json['usuario'][]=$resulta;}
     
     //echo $sentencia;
     echo json_encode($json);
--- php/reporteNoApropiado.php
+++ php/reporteNoApropiado.php
@@ -8,10 +8,12 @@ $json=array();
     
     $sentencia="delete viddoc, reportesviddoc from viddoc  INNER JOIN reportesviddoc  on viddoc.idVidDoc = reportesviddoc.idVidDoc  where reportesviddoc.reportes >= 10 and reportesviddoc.tipo = 'No es apropiado al tema o materia' ";
     // echo $sentencia;
-    while(mysqli_query($conexion, $sentencia)){
+     if(mysqli_query($conexion, $sentencia)){
     $json['usuario'][]=array("success" => 1,);
 
-}
+} else{
+          $resulta["success"]=0;
+$json['usuario'][]=$resulta;}
     
     //echo $sentencia;
     echo json_encode($json);
