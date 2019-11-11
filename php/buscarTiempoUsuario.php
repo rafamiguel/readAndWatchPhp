36712278b39a71f2d78788148277cdb9e353033e
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-11 04:56:14 -0600

    Update buscarTiempoUsuario.php

--- php/buscarTiempoUsuario.php
+++ php/buscarTiempoUsuario.php
@@ -5,7 +5,7 @@ $json=array();
 /*Comprobar si existe el usuario o no*/
         if(isset($_GET["idUsuario"])){
             $idUsuario=$_GET["idUsuario"];
-            $consulta="SELECT nombre, rutaFoto, ultimoInicio from usuario WHERE idUsuario = {$idUsuario} and tipo='E'";  
+            $consulta="SELECT nombre, rutaFoto, ultimoInicio from usuario tipo='E'";  
             $resultado=mysqli_query($conexion, $consulta);
             if($registro=mysqli_fetch_array($resultado)){
                 $json['usuario'][] =$registro; 
