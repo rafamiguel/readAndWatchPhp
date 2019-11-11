e482516e705083e84b84f52c7710abe8935e1d6c
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-10 19:08:26 -0600

    Update reporteSpam.php

--- php/reporteSpam.php
+++ php/reporteSpam.php
@@ -8,10 +8,12 @@ $json=array();
     
     $sentencia="UPDATE viddoc as a INNER JOIN reportesviddoc as b on a.idVidDoc = b.idVidDoc set a.eliminado = 'S' where b.reportes >= 5 and b.tipo = 'Es spam' ";
     // echo $sentencia;
-    while(mysqli_query($conexion, $sentencia)){
+    if(mysqli_query($conexion, $sentencia)){
     $json['usuario'][]=array("success" => 1,);
 
-}
+} else{
+          $resulta["success"]=0;
+$json['usuario'][]=$resulta;}
     
     //echo $sentencia;
     echo json_encode($json);
