9ab0cf3e9ec258d17493708175092304edd3910c
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-10 18:20:02 -0600

    aa

--- php/reporteSpam.php
+++ php/reporteSpam.php
@@ -6,7 +6,7 @@ $json=array();
 
     $idVidDoc=$_GET["idVidDoc"];
     
-    $sentencia="UPDATE viddoc as a INNER JOIN reportesviddoc as b on a.idVidDoc = b.idVidDoc set a.eliminado = 'S' where b.reportes >= 3 and b.tipo = 'Contenido sexual u obseno' ";
+    $sentencia="UPDATE viddoc as a INNER JOIN reportesviddoc as b on a.idVidDoc = b.idVidDoc set a.eliminado = 'S' where b.reportes >= 5 and b.tipo = 'Es spam' ";
     // echo $sentencia;
     while(mysqli_query($conexion, $sentencia)){
     $json['usuario'][]=array("success" => 1,);
