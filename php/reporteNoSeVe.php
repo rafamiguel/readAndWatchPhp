9b467a7abebee6d939379d2fb12a79e34f27c29a
skillerhero <skillerhero@users.noreply.github.com>
2019-11-12 23:40:40 -0600

    Update reporteNoSeVe.php

--- php/reporteNoSeVe.php
+++ php/reporteNoSeVe.php
@@ -7,15 +7,12 @@ $json=array();
     $idVidDoc=$_GET["idVidDoc"];
     
     $sentencia="UPDATE viddoc as a INNER JOIN reportesviddoc as b on a.idVidDoc = b.idVidDoc set a.eliminado = 'S' where b.reportes >= 3 and b.tipo = 'No se puede visualizar' ";
-    // echo $sentencia;
-     if(mysqli_query($conexion, $sentencia)){
+    if(mysqli_query($conexion, $sentencia)){
     $json['usuario'][]=array("success" => 1,);
 
 } else{
-          $resulta["success"]=0;
-$json['usuario'][]=$resulta;}
-    
-    //echo $sentencia;
+    $resulta["success"]=0;
+	$json['usuario'][]=$resulta;
     echo json_encode($json);
 }
 mysqli_close($conexion);
