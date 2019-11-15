c862b120922485a4652919b750215cfe641027b6
skillerhero <skillerhero@users.noreply.github.com>
2019-11-15 05:22:10 -0600

    Update cargarPregunta.php

--- php/cargarPregunta.php
+++ php/cargarPregunta.php
@@ -8,18 +8,14 @@ if(isset($_GET["fechaActual"])){
     $resultado=mysqli_query($conexion, $consulta);
     while($r=$resultado->fetch_assoc()){
 		$expire = $r["fechaSubida"];
-		echo $expire."<br>";
 		$expire_dt = new DateTime($expire);
 		$expire_dt->modify('+1 week');
-    	echo $expire."<br>";
     	$today_dt = new DateTime($fechaActual);
 	
 		if ($expire_dt < $today_dt) { 
-			echo "hola";
 			$sentencia = "delete from pregunta where idPregunta=".$r["idPregunta"];
 			mysqli_query($conexion, $sentencia);
 		 }else{
-		 	echo "adios";
         $json['usuario'][] = $r;
 
         } 
