f7dd5ce8bed556b0b7bd519048b59753b9d20123
skillerhero <skillerhero@users.noreply.github.com>
2019-11-15 05:00:19 -0600

    Update cargarPregunta.php

--- php/cargarPregunta.php
+++ php/cargarPregunta.php
@@ -16,10 +16,10 @@ if(isset($_GET["fechaActual"])){
 			mysqli_query($conexion, $sentencia);
 		 }else{
         $json['usuario'][] = $r;
-            echo json_encode($json);
+
         } 
     }
-
+    echo json_encode($json);
     mysqli_close($conexion);
 
 
