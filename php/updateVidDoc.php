1cdd4aff539ccb738302b537c4549c63cfba1cf0
skillerhero <skillerhero@users.noreply.github.com>
2019-11-25 22:40:06 -0600

    Update updateVidDoc.php

--- php/updateVidDoc.php
+++ php/updateVidDoc.php
@@ -18,9 +18,9 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["idTema"]) && isset($_GET["tipo"]) &&
     $sentencia="update viddoc set
     tipo='{$tipo}',descripcion='{$descripcion}',rutaImagen='@drawable/doc',ruta='{$ruta}',fechaSubida='{$fechaSubida}',visitas=0,idUsuario={$idUsuario},idSubtema={$idTema} where idVidDoc={$idVidDoc}";
     }
-    echo $sentencia;
     $resultado=mysqli_query($conexion, $sentencia);
-    //echo json_encode($json);
+    $json['viddoc'][]=$resultado;
+    echo json_encode($json);
 }
 mysqli_close($conexion);
 ?>
