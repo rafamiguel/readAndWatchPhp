6f1b872802890ba7040229d917211ed797ec1d43
skillerhero <skillerhero@users.noreply.github.com>
2019-11-25 22:27:48 -0600

    Update updateVidDoc.php

--- php/updateVidDoc.php
+++ php/updateVidDoc.php
@@ -3,9 +3,9 @@ include 'conexion.php';
 $json=array();
 ;
 /*Cargar los comentarios de video*/
-if(isset($_GET["idVidDoc"]) && isset($_GET["idSubtema"]) && isset($_GET["tipo"]) && isset($_GET["descripcion"])&& isset($_GET["ruta"]) && isset($_GET["fechaSubida"]) && isset($_GET["idUsuario"])){
-    $idSubtema=$_GET["idVidDoc"];
-    $idTema=$_GET["idSubtema"];
+if(isset($_GET["idVidDoc"]) && isset($_GET["idTema"]) && isset($_GET["tipo"]) && isset($_GET["descripcion"])&& isset($_GET["ruta"]) && isset($_GET["fechaSubida"]) && isset($_GET["idUsuario"])){
+    $idVidDoc=$_GET["idVidDoc"];
+    $idTema=$_GET["idTema"];
     $tipo=$_GET["tipo"];
     $descripcion=$_GET["descripcion"];
     $ruta=$_GET["ruta"];
@@ -13,10 +13,10 @@ if(isset($_GET["idVidDoc"]) && isset($_GET["idSubtema"]) && isset($_GET["tipo"])
     $idUsuario=$_GET["idUsuario"];
     if($tipo=='v'){
     $sentencia="update viddoc set
-    tipo='{$tipo}',descripcion='{$descripcion}',rutaImagen='@drawable/miniatura',ruta='{$ruta}',fechaSubida='{$fechaSubida}',visitas=0,idUsuario={$idUsuario},idSubtema={$idSubtema} where idVidDoc={$idVidDoc}";
+    tipo='{$tipo}',descripcion='{$descripcion}',rutaImagen='@drawable/miniatura',ruta='{$ruta}',fechaSubida='{$fechaSubida}',visitas=0,idUsuario={$idUsuario},idSubtema={$idTema} where idVidDoc={$idVidDoc}";
     }else{
     $sentencia="update viddoc set
-    tipo='{$tipo}',descripcion='{$descripcion}',rutaImagen='@drawable/doc',ruta='{$ruta}',fechaSubida='{$fechaSubida}',visitas=0,idUsuario={$idUsuario},idSubtema={$idSubtema} where idVidDoc={$idVidDoc}";
+    tipo='{$tipo}',descripcion='{$descripcion}',rutaImagen='@drawable/doc',ruta='{$ruta}',fechaSubida='{$fechaSubida}',visitas=0,idUsuario={$idUsuario},idSubtema={$idTema} where idVidDoc={$idVidDoc}";
     }
     $resultado=mysqli_query($conexion, $sentencia);
     //echo json_encode($json);
